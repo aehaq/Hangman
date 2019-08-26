b1df74f77d32e76cabe0ee1287068dbb3c431f09
aehaq <azfarehaq@gmail.com>
2019-08-26 12:23:21 -0700

    init file start

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,15 @@
-var words = ["human", "elf", "dwarf", "halfling", "orc", "gnome"]
+var words = ["elf", "dwarf", "halfling", "orc", "gnome", "hobbit"]
 
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+var winCount, lossCount = 0;
+var wordChoice, wordDisplayed, guessesLeft;
+
+function init() {
+    guessesLeft = 10;
+    wordChoice = words[Math.floor(Math.random()*words.length)];
+    wordDisplayed = "";
+    for (var i = 0; i < wordChoice.length; i++) {
+        wordDisplayed += "_"
+    }
+}
