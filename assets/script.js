6a09ef42c5e99f49679cb399ec028293adf1dd34
aehaq <azfarehaq@gmail.com>
2019-08-26 12:56:33 -0700

    word guess print improvement

--- assets/script.js
+++ assets/script.js
@@ -5,11 +5,32 @@ var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 var winCount, lossCount = 0;
 var wordChoice, wordDisplayed, guessesLeft;
 
+var guessesElement = document.getElementById("letters-guessed");
+var displayedElement = document.getElementById("displayed-word");
+var targetElement = document.getElementById("target-word");
+console.log(targetElement)
+
 function init() {
     guessesLeft = 10;
     wordChoice = words[Math.floor(Math.random()*words.length)];
-    wordDisplayed = "";
+    targetElement.textContent = wordChoice;
+    wordProgress = "";
     for (var i = 0; i < wordChoice.length; i++) {
-        wordDisplayed += "_"
+        wordProgress += "_";
     }
+    convertString(wordProgress);
 }
+
+function convertString(string) {
+    wordDisplayed = "";
+    for (let i = 0; i < string.length; i++) {
+        wordDisplayed += string[i];
+        if (i != string.length - 1) {
+            wordDisplayed += " "
+        }
+    }
+    displayedElement.textContent = wordDisplayed
+}
+
+
+init()
