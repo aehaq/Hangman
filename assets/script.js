b7c3cb4dfd8cddae895e8feed7ca524972423419
aehaq <azfarehaq@gmail.com>
2019-08-27 11:04:04 -0700

    fixed display errors, added onkeyup function

--- assets/script.js
+++ assets/script.js
@@ -2,18 +2,26 @@ var words = ["elf", "dwarf", "halfling", "orc", "gnome", "hobbit"]
 
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-var winCount, lossCount = 0;
+var winCount = 0;
+var lossCount = 0;
 var targetWord, wordDisplayed, guessesLeft, lettersGuessed;
 
 var guessesElement = document.getElementById("letters-guessed");
 var displayedElement = document.getElementById("displayed-word");
+var winElement = document.getElementById("win-count");
+var lossElement = document.getElementById("loss-count");
+var leftElement = document.getElementById("guesses-left");
 
 function init() {
     guessesLeft = 10;
     lettersGuessed = [];
-    guessesElement.textContent = lettersGuessed;
     targetWord = words[Math.floor(Math.random()*words.length)];
-    console.log(targetWord)
+
+    guessesElement.textContent = lettersGuessed;
+    leftElement.textContent = guessesLeft;
+    winElement.textContent = winCount;
+    lossElement.textContent = lossCount;
+
     wordProgress = "";
     for (var i = 0; i < targetWord.length; i++) {
         wordProgress += "_";
@@ -32,14 +40,17 @@ function convertString(string) {
     displayedElement.textContent = wordDisplayed
 }
 
-document.onkeyup = function(event) {
+document.onkeydown = function(event) {
     var guess = event.key;
     console.log(guess)
 
     // Check if guess is valid letter
     if (letters.indexOf(guess) > -1 && lettersGuessed.indexOf(guess) < 0) {
         lettersGuessed.push(guess);
+        guessesLeft -= 1;
+
         guessesElement.textContent = lettersGuessed;
+
         for (var i = 0; i < targetWord.length; i++) {
             if (targetWord[i] == guess) {
                 wordProgress = wordProgress.substring(0, i) + guess + wordProgress.substring(i+1);              
@@ -52,10 +63,21 @@ document.onkeyup = function(event) {
         console.log("Guess a letter that has not been guessed yet")
     }
     
+}
+
+document.onkeyup = function(event) {
+
     if (wordProgress == targetWord) {
         alert("YOU WON!")
-        init();
         winCount += 1
+        init();
+    } else {
+        
+        leftElement.textContent = guessesLeft;
+        if (guessesLeft < 1) {
+            lossCount += 1;
+            init();
+        }
     }
 }
 
