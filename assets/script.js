13b1a3215a076e20e4ecefa06a12e1655eedf699
aehaq <azfarehaq@gmail.com>
2019-10-05 09:59:38 -0700

    styling added

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var words = ["elf", "dwarf", "halfling", "orc", "gnome", "hobbit"]
+var words = ["elf", "dwarf", "halfling", "orc", "gnome", "goblin", "hobbit"]
 
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
